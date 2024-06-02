0eb98a625238cea0dd025d89fc0310dede175fc5
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-02 19:10:16 +0300

    Create coupon entity

---
+++ Services/Coupons/Core/Coupons.Domain/Entities/Coupon.cs
@@ -0,0 +1,16 @@
+﻿namespace Coupons.Domain.Entities;
+
+public sealed class Coupon
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string CouponCode { get; set; } = string.Empty;
+
+    public string DiscountAmount { get; set; } = string.Empty;
+
+    public string MinAmount { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime CouponValidityPeriod { get; set; }
+}
