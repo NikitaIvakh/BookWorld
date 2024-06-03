c7ced01c18904035ef1d21523791c78654a6c21f
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-03 20:43:25 +0300

    Create DependencyInjection class

---
+++ Services/Coupons/Infrastructure/Coupons.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coupons.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        ConfigureRepositories(services);
+        ConfigureDatabase(services, configuration);
+        ApplyMigrations(services);
+    }
+
+    private static void ConfigureRepositories(IServiceCollection services)
+    {
+
+    }
+
+    private static void ConfigureDatabase(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+
+    private static void ApplyMigrations(this IServiceCollection services)
+    {
+        var builder = services.BuildServiceProvider();
+        var scope = builder.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (db.Database.GetPendingMigrations().Any())
+        {
+            db.Database.Migrate();
+        }
+    }
+}
