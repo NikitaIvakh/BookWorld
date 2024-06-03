6bfe40c9784a2b3e7e0b575368e4aa785c4c6934
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-03 20:31:03 +0300

    Create ApplicationDbContext class

---
+++ Services/Coupons/Core/Coupons.Application/DbContexts/ICouponDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Coupons.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coupons.Application.DbContexts;
+
+public interface ICouponDbContext
+{
+    public DbSet<Coupon> Coupons { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken token = default);
+}
---
+++ Services/Coupons/Infrastructure/Coupons.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Coupons.Application.DbContexts;
+using Coupons.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coupons.Infrastructure;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), ICouponDbContext
+{
+    public DbSet<Coupon> Coupons { get; private init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
