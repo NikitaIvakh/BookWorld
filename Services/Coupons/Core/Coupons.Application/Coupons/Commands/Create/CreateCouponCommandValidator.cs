68df176e4f5d131c00a8becbf560cffea55cfb4d
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-08 19:45:41 +0300

    Add CreateCouponCommandValidator class

---
+++ Services/Coupons/Core/Coupons.Application/Coupons/Commands/Create/CreateCouponCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Coupons.Domain.Common;
+using FluentValidation;
+
+namespace Coupons.Application.Coupons.Commands.Create;
+
+public class CreateCouponCommandValidator : AbstractValidator<CreateCouponCommand>
+{
+    public CreateCouponCommandValidator()
+    {
+        RuleFor(key => key.CouponCode).NotEmpty().MaximumLength(Constraints.MAXIMUM_LENGTH);
+        RuleFor(key => key.DiscountAmount).NotEmpty();
+        RuleFor(key => key.MinAmount).NotEmpty();
+        RuleFor(key => key.CouponValidityPeriod).NotEmpty();
+    }
+}
