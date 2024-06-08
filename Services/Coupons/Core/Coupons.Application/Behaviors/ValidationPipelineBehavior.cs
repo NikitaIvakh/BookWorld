2cfe096a5f8453a247b2d84be09f92409f67b71e
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-08 19:42:08 +0300

    Create ValidationPipelineBehavior class

---
+++ Services/Coupons/Core/Coupons.Application/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,55 @@
+﻿using Coupons.Domain.Shared;
+using FluentValidation;
+using MediatR;
+
+namespace Coupons.Application.Behaviors;
+
+public sealed class ValidationPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!validators.Any())
+        {
+            return await next();
+        }
+
+        var errors = validators
+            .Select(validator => validator.Validate(request))
+            .SelectMany(validationResult => validationResult.Errors)
+            .Where(validationFailure => validationFailure is not null)
+            .Select(failure => new Error
+                (
+                    failure.PropertyName,
+                    failure.ErrorMessage)
+                )
+            .Distinct()
+            .ToArray();
+
+        if (errors.Any())
+        {
+            return CreateValidationResult<TResponse>(errors);
+        }
+
+        return await next();
+    }
+
+    private static TResult CreateValidationResult<TResult>(Error[] errors)
+        where TResult : Result
+    {
+        if (typeof(TResult) == typeof(Result))
+        {
+            return (ValidationResult.WithErrors(errors) as TResult)!;
+        }
+
+        var validationResult = typeof(ValidationResultT<>)
+             .GetGenericTypeDefinition()
+             .MakeGenericType(typeof(TResult).GenericTypeArguments[0])
+             .GetMethod(nameof(ValidationResult.WithErrors))!
+             .Invoke(null, [errors])!;
+
+        return (TResult)validationResult;
+    }
+}
