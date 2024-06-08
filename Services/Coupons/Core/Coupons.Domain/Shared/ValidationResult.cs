1c7f8863ea30697f7f94bfeaca01b7f180198851
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-08 19:19:23 +0300

    Add base validation classes

---
+++ Services/Coupons/Core/Coupons.Domain/Shared/ValidationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Coupons.Domain.Shared;
+
+public sealed class ValidationResult : Result, IValidationResult
+{
+    private ValidationResult(Error[] error) : base(false, IValidationResult.ValidationError)
+    {
+        Errors = error;
+    }
+
+    public Error[] Errors { get; }
+
+    public static ValidationResult WithErrors(Error[] error) => new(error);
+}
---
+++ Services/Coupons/Core/Coupons.Domain/Shared/ValidationResultT.cs
@@ -0,0 +1,13 @@
+﻿namespace Coupons.Domain.Shared;
+
+public sealed class ValidationResultT<TValue> : ResultT<TValue>, IValidationResult
+{
+    private ValidationResultT(Error[] errors) : base(default, false, IValidationResult.ValidationError)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; }
+
+    public static ValidationResultT<TValue> WithErrors(Error[] errors) => new(errors);
+}
