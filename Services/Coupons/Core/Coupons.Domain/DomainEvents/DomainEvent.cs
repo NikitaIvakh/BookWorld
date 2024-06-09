3c351fb9f72e9d084b8de28a109bad34c2b45f7e
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-09 19:20:30 +0300

    Create domainEvent interface

---
+++ Services/Coupons/Core/Coupons.Domain/DomainEvents/DomainEvent.cs
@@ -0,0 +1,5 @@
+﻿using Coupons.Domain.Primitives;
+
+namespace Coupons.Domain.DomainEvents;
+
+public abstract record DomainEvent(Guid Id) : IDomainEvent;
---
+++ Services/Coupons/Core/Coupons.Domain/Primitives/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Coupons.Domain.Primitives;
+
+public interface IDomainEvent : INotification
+{
+    public Guid Id { get; init; }
+}
