1b3b6592cc8b1f919c3c652628dc9ff91a997778
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-09 19:44:25 +0300

    Create IDomainEventHandler interface

---
+++ Services/Coupons/Core/Coupons.Application/Abstractors/Messages/EventHandlers/IDomainEventHandler.cs
@@ -0,0 +1,6 @@
+﻿using Coupons.Domain.Primitives;
+using MediatR;
+
+namespace Coupons.Application.Abstractors.Messages.EventHandlers;
+
+public interface IDomainEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IDomainEvent;
