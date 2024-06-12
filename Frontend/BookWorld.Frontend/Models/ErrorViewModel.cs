ad32e3ab223499c6866730fd9e5cf84e6ab9d3aa
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-12 20:42:04 +0300

    Add Frontend project

---
+++ Frontend/BookWorld.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookWorld.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
