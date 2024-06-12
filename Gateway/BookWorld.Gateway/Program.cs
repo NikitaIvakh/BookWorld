4809711b7db7ea9cf2741be984b6b34faae6a902
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-12 20:28:44 +0300

    Add gateway and get logic for get coupons with gateway

---
+++ Gateway/BookWorld.Gateway/Program.cs
@@ -0,0 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseOcelot().GetAwaiter().GetResult();
+app.Run();
