055c9708581f89be4fdc922320e645f94449b0e2
Nikita Ivakh <maffy145rus@mail.ru>
2024-06-12 20:43:54 +0300

    Update Frontend project

--- Frontend/BookWorld.Frontend/Controllers/HomeController.cs
+++ Frontend/BookWorld.Frontend/Controllers/HomeController.cs
@@ -2,31 +2,23 @@ using BookWorld.Frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookWorld.Frontend.Controllers
+namespace BookWorld.Frontend.Controllers;
+
+public sealed class HomeController(ILogger<HomeController> logger) : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
+        return View();
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
